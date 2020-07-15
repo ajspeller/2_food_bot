338ec581248f4dd14c40815315490e765620b631
AJ Speller <ajspeller@gmail.com>
2020-07-15 03:55:47 -0400

    working with json

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const commandLineArgs = require('command-line-args');
+const fs = require('fs');
 
 const optionsDefinitions = [
   { name: 'name', type: String },
@@ -7,7 +8,7 @@ const optionsDefinitions = [
   { name: 'exit', type: Boolean },
 ];
 
-const { name, order, payment, exit } = commandLineArgs(optionsDefinitions);
+let { name, order, payment, exit } = commandLineArgs(optionsDefinitions);
 
 // node app.js
 // node app.js --name=ajspeller
@@ -15,16 +16,36 @@ const { name, order, payment, exit } = commandLineArgs(optionsDefinitions);
 // node app.js --payment=100
 // node app.js --exit
 
+let getJson = fs.readFileSync('db.json');
+const data = JSON.parse(getJson);
+
+const saveIt = (newData) => {
+  fs.writeFileSync('db.json', JSON.stringify(newData));
+};
+
 if (name) {
   console.log(`hello, ${name}, we are serving cake, pizza and salad`);
+  data.name = name;
+  saveIt(data);
 } else if (order) {
-  console.log(`ok, that would be $25, you will pay with ...`);
+  console.log(`ok, ${data.name} that would be $25, you will pay with ...`);
+  data.order = order;
+  saveIt(data);
 } else if (payment) {
+  data.payment = payment;
   console.log(
-    `your change is ..... thanks for eating with us .. type --exit to get the order`
+    `your change is ${
+      payment - 25
+    } thanks for eating with us .. type --exit to get the order`
   );
+  saveIt(data);
 } else if (exit) {
+  console.log(`data: `, data);
   console.log(`thanks`);
+  for (prop in data) {
+    data[prop] = '';
+  }
+  saveIt(data);
 } else {
   console.log(`hello, please enter your name`);
 }
